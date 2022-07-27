f920b8480fe144c9057efe3a6125e5e88eca247a
Vadim <Vadim@LAPTOP-2QGAUP8V.(none)>
2022-07-27 17:22:12 +0300

    Create nginx and gunicorn.

--- Dockerfile
+++ Dockerfile
@@ -7,3 +7,7 @@ WORKDIR /app
 COPY requirements.txt /app/
 RUN pip install -r requirements.txt
 COPY . /app/
+
+WORKDIR ./
+
+RUN python manage.py collectstatic
--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt import views as jwt_views
 from rest_framework_swagger.views import get_swagger_view
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 schema_view = get_swagger_view(title='Pastebin API')
 
@@ -16,3 +17,4 @@ urlpatterns = [
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
+urlpatterns += staticfiles_urlpatterns()
